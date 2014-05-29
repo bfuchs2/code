42fa53d19d47f08990b007144185a883eaaa9bd0
bfuchs2 <bernardtfuchs@gmail.com>
2014-05-29 12:36:40 -0700

    improved graphics but still very buggy

--- main.rb
+++ main.rb
@@ -29,6 +29,7 @@ class Main < Gosu::Window
     @kbDownTime = Time.now
     @score = 0
     @moving = false
+    @movement = 0
   end
 
   def placeRandomTile(board = @board)
@@ -69,43 +70,114 @@ class Main < Gosu::Window
   #increment determines if the score should be changed by this move (if it's actually happening or just
   #the computer thinking)
   def move dir, increment = false, board = @board, score = @score
-    newScore = 0
+    newScore = 0 if increment
     newBoard = Array.new(4){ |x|
       Array.new(board[x])
     }
+    if increment
+      @moveFactor = Array.new(4){ |x|
+        Array.new(4, 0)  
+      }
+    end
     if dir == Dir::Up
       for row in 0...4
         unless newBoard[row] == Array.new(4, 0)
-          newBoard[row].delete(0)
-          newBoard[row].each_index{ |i|
-            if newBoard[row][i] == newBoard[row][i+1]
-            newScore += newBoard[row][i]
-            newBoard[row][i] *= 2
-            newBoard[row].delete_at(i + 1)
+          #rather than deleting the zeroes, all the following things need to be kept track of for movement
+          if increment
+            posn = 0
+            while posn < 4
+              if newBoard[row][posn] == 0
+                for inc in posn+1..3
+                  @moveFactor[row][inc]+= 1
+                end
+                newBoard[row].delete_at(posn)
+              else
+                posn+=1
+              end
             end
-          }
+          else
+            newBoard[row].delete(0) #but if nothing is being changed, we do it this way 
+          end
+          #now it's time to combine similar tiles
+          if increment
+            newBoard[row].each_index{ |i|
+              if newBoard[row][i] == newBoard[row][i+1]
+                newScore += newBoard[row][i]
+                newBoard[row][i] *= 2
+                @moveFactor[row].each_index do |inc|
+                  if inc > i
+                    @moveFactor[row][inc] += 1
+                  end
+                end
+                newBoard[row].delete_at(i + 1)
+              end
+            }
+          else
+            newBoard[row].each_index{ |i|
+              if newBoard[row][i] == newBoard[row][i+1]
+                newBoard[row][i] *= 2
+                newBoard[row].delete_at(i + 1)
+              end
+            }
+          end
           newBoard[row].concat(Array.new(4-newBoard[row].size, 0))
         end
       end
       @score += newScore if increment
     elsif dir == Dir::Right
       newBoard = move Dir::Up, increment, rotate
-      3.times{newBoard = rotate newBoard}
+      3.times{
+        newBoard = rotate newBoard
+        @moveFactor = rotate @moveFactor
+      }
     elsif dir == Dir::Left
       newBoard = move Dir::Down, increment, rotate
-      3.times{newBoard = rotate newBoard}
+      3.times{
+        newBoard = rotate newBoard
+        @moveFactor = rotate @moveFactor
+      }
     elsif dir == Dir::Down
       for row in 0...4
         unless newBoard[row] == Array.new(4, 0)
-          newBoard[row].delete(0)
-          newBoard[row].each_index{ |i|
-            if newBoard[row][newBoard[row].size-i] == newBoard[row][newBoard[row].size-1-i]
-            newScore = newBoard[row][newBoard[row].size-i]
-            newBoard[row][newBoard[row].size-i] *= 2
-            newBoard[row].delete_at(newBoard[row].size-1-i)
+          #rather than deleting the zeroes, all the following things need to be kept track of for movement
+          if increment
+            posn = 3
+            while posn >= 0
+              if newBoard[row][posn] == 0
+                for inc in 0..posn
+                  @moveFactor[row][inc]+= 1
+                end
+                newBoard[row].delete_at(posn)
+              else
+                posn-=1
+              end
             end
-          }
-          (4-newBoard[row].size).times{ newBoard[row].unshift(0)}
+          else
+            newBoard[row].delete(0) #but if nothing is being changed, we do it this way 
+          end
+          #now it's time to combine similar tiles
+          if increment
+            newBoard[row].each_index{ |i|
+              if newBoard[row][-i-1] == newBoard[row][-i-2]
+                newScore += newBoard[row][-1]
+                newBoard[row][-i-1] *= 2
+                @moveFactor[row].each_index do |inc|
+                  if inc <= i
+                    @moveFactor[row][inc] += 1
+                  end
+                end
+                newBoard[row].delete_at(-i-2)
+              end
+            }
+          else
+            newBoard[row].each_index{ |i|
+              if newBoard[row][i] == newBoard[row][i+1]
+                newBoard[row][i] *= 2
+                newBoard[row].delete_at(i + 1)
+              end
+            }
+          end
+          (4-newBoard[row].size).times{newBoard[row].unshift(0)}
         end
       end
       @score += newScore if increment
@@ -137,21 +209,26 @@ class Main < Gosu::Window
   end
 
   def tileCoords(x, y)
-    if !@moving or @board[x][y] == @futureBoard[x][y]
+    if !@moving or @moveFactor[x][y] == 0
       [x*@tilesize+20, y*@tilesize+50]
     else
-      moveAmount = (Time.now-@kbDownTime)*@tilesize/@@WAIT
+      moveAmount = @movement*@moveFactor[x][y]
       case @moveDir
-      when Dir::Up then [x*@tilesize+20, y*@tilesize+50-moveAmount]
-      when Dir::Right then [x*@tilesize+20+moveAmount, y*@tilesize+50]
-      when Dir::Down then [x*@tilesize+20, y*@tilesize+50+moveAmount]
-      when Dir::Left then [x*@tilesize+20-moveAmount, y*@tilesize+50]
+      when Dir::Up then 
+        [x*@tilesize+20, y*@tilesize+50-moveAmount]
+      when Dir::Right then 
+        [x*@tilesize+20+moveAmount, y*@tilesize+50]
+      when Dir::Down then 
+        [x*@tilesize+20, y*@tilesize+50+moveAmount]
+      when Dir::Left then 
+        [x*@tilesize+20-moveAmount, y*@tilesize+50]
       end
     end
   end
   
   def draw
     @smallFont.draw("score:#{@score}", 20, 5, 1, 1.0, 1.0, 0xf0f0f000)
+    @movement += 1
     @board.each_index do |x|
       @board[x].each_index do |y|
         unless @board[x][y] == 0
@@ -160,15 +237,16 @@ class Main < Gosu::Window
       end
     end
     if gameOver
-      @font.draw("YOU SUCK", 20, 2*@tilesize-50, 1, 1.0, 1.0, 0xffff0000)
-      @smallFont.draw("your final score was #{@score}", 20, 2*@tilesize+15, 1, 1.0, 1.0, 0xf0f00000)
+      @font.draw("YOU SUCK", 20, 2*@tilesize, 1, 1.0, 1.0, 0xffff0000)
+      @smallFont.draw("your final score was #{@score}", 20, 2.5*@tilesize, 1, 1.0, 1.0, 0xf0f00000)
     end
     #I know you aren't supposed to do computation in the draw method
     #but this makes it so much easier
-    if @moving and Time.now - @kbDownTime > @@WAIT
+    if @moving and @movement >= @tilesize
       @moving = false
       @oldBoards.push @board
       @board = @futureBoard
+      @movement = 0
       placeRandomTile
     end
   end
