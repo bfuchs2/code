7b0b0ebc85d1206b384b21072cd543a43f2f6f8d
bfuchs2 <bernardtfuchs@gmail.com>
2014-06-14 23:10:28 -0700

    fixed some sorting functions and removed the other ones. removed parent link since it was never really used

--- LinkedList.rb
+++ LinkedList.rb
@@ -2,25 +2,23 @@
 #a semi-functional linked list implementation in Ruby
 #I wrote this to help me learn Ruby then stopped working on it
 class LL  
- attr_accessor :parent, :object, :child
+ attr_accessor :object, :child
   
   def get(i)
-    if( i < 0)
-      return @parent.get(i + 1)
-    elsif(i == 0)
-      @object
+    if(i == 0)
+      return @object
     else
-      @child.get(i - 1)
+      return @child.get(i - 1)
     end
   end
   
   def getLink(i)
-    if(i < 0)
-      return @parent.getLink(i + 1)
-    elsif(i == 0)
-      self
+    if(i == 0)
+      return self
+    elsif(@child == nil)
+      return nil
     else
-      @child.getLink(i-1)
+      return @child.getLink(i-1)
     end
   end
   
@@ -45,7 +43,6 @@ class LL
       #removes all pointers to the child object
       if(@child)
         @child = @child.child
-        @child.parent = self 
       end
     elsif(@child)
       @child.remove(i - 1)
@@ -61,16 +58,17 @@ class LL
     end
   end
   
-  def initialize(object, parent = nil, child = nil)
-    if(object.is_a? Array)
-      @object = object.shift
-      if(!object.empty?)
-        @child = LL.new(object, self)
+  def initialize(obj, child = nil)
+    if(obj.is_a? Array)
+      @object = obj[0]
+      obj.shift
+      if not obj.empty?
+        @child = LL.new(obj)
       end
+    else
+      @object = obj
+      @child = child
     end
-    @object = object
-    @parent = parent
-    @child = child
   end
   
   def toArray
@@ -81,28 +79,23 @@ class LL
     end
   end
   
-  def self.moveTo(topLink, from, to) 
-    link = topLink.getLink(from)
-    topLink.remove(from)
-    topLink.insert(link.object, to)
-  end
-  
   def add(object)
     if(@child)
       @child.add(object)
     else
-      @child = LL.new(object, self)
+      @child = LL.new(object)
     end
-    toArray
+  end
+  
+  def push(object)
+    add(object)
   end
   
   def linkInsert(link, index)
     if(index == 0)#self becomes link, child becomes self
-      return @parent.linkInsert(link, 1) if(@parent)
-      @child = LL.new(@object, self, @child)
+      @child = LL.new(@object, @child)
       @object = link.object
     elsif(index == 1)#insert at child
-      link.parent = self
       link.child = @child
       @child = link
     elsif(@child == nil)
@@ -114,18 +107,15 @@ class LL
 
   def insert(object, index)
     if(index == 0)
-      temp = @child
-      @child = LL.new(@object, self, temp)
+      @child = LL.new(@object, @child)
       @object = object
-      return toArray
-    end
-    if(@child == nil)
-      @child = LL.new(object, self)
+    elsif(@child == nil)
+      @child = LL.new(object)
     elsif(index > 1)
       @child.insert(object, index - 1)
     else
       temp = child
-      @child = LL.new(object, self, temp)
+      @child = LL.new(object, temp)
     end
     toArray
   end
@@ -141,51 +131,47 @@ class LL
         comp = 1
       end
     end
+    comp
   end
   
-  def self.sSort(topLink, lo = 0, hi = topLink.length-1)
-      #rather than swapping elements, this method will simply move them
-      return if(lo == hi)
-      iter = smallestLink = topLink.getLink(lo)
-      while(iter.child)
-        iter = iter.child
-        smallestLink = iter if smallestLink.compareTo(iter) == 1
-      end
-      print smallestLink.parent.object, ", " if smallestLink.parent
-      print smallestLink.object, ", "
-      print smallestLink.child.object, ", " if smallestLink.child
-      if(smallestLink != topLink.getLink(lo))
-        topLink.linkInsert(smallestLink, lo)
-        #topLink.linkRemove(smallestLink)
-        smallestLink.parent.child = smallestLink.child if(smallestLink.parent)
-        smallestLink.child.parent = smallestLink.parent if(smallestLink.child)
-      end
-      print topLink.toArray, "\n"
-      sSort(topLink, lo+1, hi)   
+  #the following two methods are because I kept forgetting how compareTo works
+  def greaterThan(other)
+    return compareTo(other) == 1
   end
   
-  def self.qSort(topLink)
-    pivot = topLink.getLink(topLink.length()/2)
-    for i in 0..topLink.length-1
-      toSort = topLink.getLink(i)
-      if(toSort.compareTo(pivot) == 1) 
+  def lessThan(other)
+    return compareTo(other) == -1
+  end
+  
+  def linkSwap(one, two)
+    one.object, two.object = two.object, one.object
+  end
+  
+  def self.bucketSort(topLink)
+    i = topLink
+    while i.child
+      if(i.greaterThan(i.child))
+        topLink.linkSwap(i, i.child)
+        i = topLink
+      else
+        i = i.child
       end
     end
+    return topLink
   end
   
-  def self.iSort(topLink)
-    for i in 1..topLink.length-1
-      toSort = topLink.getLink(i)
-      topLink.remove(i)
-      for p in 0..i-1
-        comp = topLink.getLink(p)
-        if toSort.compareTo(comp) == 1 #note: -1 --> 'toSort' before 'comp' and vice versa
-          #comp before toSort: make toSort comp's child 
-          topLink.linkInsert(toSort, p)
-          break
-        end
+  def self.selectionSort(topLink)
+    place = topLink
+    while place
+      min = i = place
+      while i
+        min = i if i.lessThan(min)
+        i = i.child
       end
+      topLink.linkSwap(place, min)
+      place = place.child
     end
+    topLink
   end
   
   def self.generate(i = 15, max = 100)
@@ -198,8 +184,3 @@ class LL
   end
 
 end
-
-ll = LL.generate(5)
-print ll.toArray, "\n"
-LL.iSort(ll)
-puts ll.toArray
