b933f09539759121ee4015b50e7ab26cd8054ec5
bfuchs2 <bernardtfuchs@gmail.com>
2015-02-18 17:05:37 -0600

    added comments

--- statistics.rb
+++ statistics.rb
@@ -1,5 +1,9 @@
 load 'euler_general.rb'
 
+#module containing basic statistical functions
+#functions that use the generic functor support x values that are integers, ranges, or left blank
+#@author bfuchs2
+#Spring 2015
 module Stat
   def Stat.populate(*x)
     ret = Array.new
@@ -35,7 +39,7 @@ module Stat
   end
   
   def Stat.nPr(n, r)
-    Eg.factorial(n)/Eg.factorial(n-r)
+    return Eg.factorial(n)/Eg.factorial(n-r)
   end
   
   def Stat.nCr(n, r)
@@ -48,6 +52,9 @@ module Stat
   
   class G
     #private method for handling various values for k
+    #when k is nil, gives an array for all expected values of k
+    #when k is a number, evaluates the given code block at k
+    #when k is a range or array, gives the sum of all code block values for all values of k
     def self.generic(k, size)
       case k
       when nil
@@ -63,29 +70,50 @@ module Stat
   end
   
   def Stat.binom(n, p, k = nil)
-    return G.generic(k, n+1){|x| prob(n, p, x)}
+    return G.generic(k, n+1){
+    |x| 
+      prob(n, p, x)
+    }
   end
   
-  def Stat.poisson(x, lambda)
-    return (lambda**x)/(Eg.factorial(x)*(Math::E**lambda))
+  def Stat.poisson(lambda, x = nil)
+    G.generic(x, lambda*2){
+    |xi|
+      (lambda**xi)/(Eg.factorial(xi)*(Math::E**lambda))
+    }
   end
   
   #returns probability that the first success will occur on the xth trial
   def Stat.geom(p, x = nil)
-    G.generic(x, 4.0/p){|k| (1-p)**(k-1)*p}
+    G.generic(x, 4.0/p){
+    |xi| 
+      (1-p)**(xi-1)*p
+    }
   end
   
   #returns probability that the kth success will occur on the xth trial
   def Stat.negBinom(p, k, x = nil)
-    G.generic(x, 2.0*k/p){|xi| nCr(xi-1, k-1)*(p**k)*((1-p)**(xi-k))}  
+    G.generic(x, 2.0*k/p){
+    |xi| 
+      nCr(xi-1, k-1)*(p**k)*((1-p)**(xi-k))
+    }  
   end
   
+  #without replacement
+  #if bigN and bigS are large enough,
+  #hypergeom can be approximated by the binomial distribution
   def Stat.hypergeom(bigN, bigS, n, x = nil)
-    G.generic(x, n+1){|xi| 1.0*nCr(bigS, xi)*nCr(bigN-bigS, n-xi)/nCr(bigN, n)}
+    G.generic(x, n+1){
+    |xi| 
+      1.0*nCr(bigS, xi)*nCr(bigN-bigS, n-xi)/nCr(bigN, n)
+    }
   end
   
   #returns probability that xi number of outcomes will occur
-  #note that in this case, pi and xi are arrays 
+  #note that in this case, pi and xi are arrays
+  #the values in pi should sum to 1
+  #the values in xi should sum to n
+  #this is NOT checked at runtime 
   def Stat.multinom(pi, xi, n)
     bottom = 1.0
     xi.each{|xii| bottom *= Eg.factorial(xii)}
